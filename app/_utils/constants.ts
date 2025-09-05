93f714769a72792f172df47b2f915ea4da787cd2
Yasser-Bouyanfif <yasser.bouyanfif@gmail.com>
2025-09-05 14:06:28 +0200

    frujfosr

--- app/_utils/constants.ts
+++ app/_utils/constants.ts
@@ -1,3 +1,3 @@
 export const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
-export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
+export const API_URL = process.env.NEXT_PUBLIC_REST_API_URL;
+export const API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
