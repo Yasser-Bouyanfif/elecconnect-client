26fe1886bea42f7eb005c0fd7ac6177384c756e0
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2025-09-25 23:45:58 +0200

    POINT

--- app/orders/page.tsx
+++ app/orders/page.tsx
@@ -42,7 +42,7 @@ interface OrderLine {
 }
 
 type OrderStatus = 'paid' | 'processing' | 'shipped' | 'delivered' | 'canceled' | 'refunded';
-
+//
 type Order = {
   id: number;
   orderNumber: string;
