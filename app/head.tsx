c5b1150b8902f5d17e502434f81d7190fc00ae94
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2025-09-29 13:03:59 +0200

    ifjjiirfjifjif

---
+++ app/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+    return (
+      <>
+        <title>ELEC'CONNECT</title>
+        <meta name="description" content="Solutions de recharge électrique" />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="ELEC'CONNECT" />
+        <meta property="og:description" content="Solutions de recharge électrique" />
+      </>
+    );
+  }
+  
