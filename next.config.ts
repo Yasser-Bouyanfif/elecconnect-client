e83b0aa6d0613810515623c4b711eba5605c1520
Yasser Bouyanfif <yasser.bouyanfif@gmail.com>
2025-10-02 13:19:17 +0200

    kfrfokfrokfr

--- next.config.ts
+++ next.config.ts
@@ -15,6 +15,12 @@ const nextConfig: NextConfig = {
         hostname: "lh3.googleusercontent.com",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "serverelec.duckdns.org",
+        port: "",
+        pathname: "/uploads/**",
+      },
     ],
   },
   typescript: {
